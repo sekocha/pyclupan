6637ff143b10972aa886db204aeab37d6f16d30e
Atsuto Seko <sekocha@gmail.com>
2022-01-12 23:10:34 +0900

    cluster.py is implemented

--- dd/cluster.py
+++ dd/cluster.py
@@ -86,6 +86,7 @@ class Cluster:
 
         if n_body_ub > 1:
             for n_body in range(2,n_body_ub+1):
+                print("  searching for "+str(n_body)+"-body clusters ...")
                 cut = cutoff[n_body-2]
                 clusters_cutoff = []
                 for cl, s in itertools.product(clusters_small, sites):
@@ -105,6 +106,8 @@ class Cluster:
 
                 clusters_small = sorted(cl_rep)
                 clusters.extend(clusters_small)
+                print("  ", len(clusters_small), 
+                      str(n_body)+"-body clusters are found.")
 
         print(' number of nonequivalent clusters (< cutoff) =', len(clusters))
 
