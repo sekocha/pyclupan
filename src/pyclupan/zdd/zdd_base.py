85b79dd7d98af2eaa27f47c4d9b160f3de87ac22
Atsuto Seko <sekocha@gmail.com>
2025-01-10 23:39:57 +0900

    Initialize pyclupan structure

--- src/pyclupan/zdd/zdd_base.py
+++ src/pyclupan/zdd/zdd_base.py
@@ -1,22 +1,171 @@
 """Class for handling ZDD."""
 
 import itertools
+from dataclasses import dataclass
 from typing import Optional
 
 import numpy as np
 
 
-class Site:
+def _compose_node(site_idx: int, element_idx: int):
+    """Return node ID from site and element IDs."""
+    return int(element_idx * 1000 + site_idx)
 
-    def __init__(self, idx, ele, ele_dd):
 
-        self.idx = idx
-        self.ele = ele
-        self.ele_dd = ele_dd
-        if len(ele) == len(ele_dd):
+def _decompose_node(node_idx: int):
+    """Return site and element IDs from node ID."""
+    site_idx = _decompose_node_to_site(node_idx)
+    element_idx = _decompose_node_to_element(node_idx)
+    return site_idx, element_idx
+
+
+def _decompose_node_to_site(node_idx: int):
+    """Return site ID from node ID."""
+    site_idx = node_idx % 1000
+    return site_idx
+
+
+def _decompose_node_to_element(node_idx: int):
+    """Return element ID from node ID."""
+    element_idx = node_idx // 1000
+    return element_idx
+
+
+@dataclass
+class ZDDSite:
+    """Dataclass for site attributes.
+
+    Parameters
+    ----------
+    idx:
+    ele:
+    ele_dd:
+    one_of_k: Use one-of-k representation.
+    """
+
+    site_idx: int
+    ele: list[int]
+    ele_dd: list[int]
+    one_of_k: bool = False
+    active: bool = False
+
+    def __post_init__(self):
+        """Post init method."""
+        if len(self.ele) == len(self.ele_dd):
             self.one_of_k = True
-        else:
-            self.one_of_k = False
+        if len(self.ele_dd) > 0:
+            self.active = True
+
+
+@dataclass
+class ZDDSiteSet:
+    """Dataclass for a set of site attributes.
+
+    Parameters
+    ----------
+    site_attrs: List of ZDDSite for the entire lattice.
+    """
+
+    n_sites: list
+    elements_lattice: list
+    one_of_k_rep: bool = False
+    inactive_elements: Optional[list] = None
+    site_attrs: Optional[list[ZDDSite]] = None
+
+    nodes: Optional[list] = None
+    active_nodes: Optional[list] = None
+    inactive_nodes: Optional[list] = None
+    sites: Optional[list] = None
+    active_sites: Optional[list] = None
+    elements: Optional[list] = None
+    active_elements: Optional[list] = None
+    active_elements_dd: Optional[list] = None
+
+    def __post_init__(self):
+        """Post init method."""
+        self.n_total_sites = sum(self.n_sites)
+        self.nodes = self._set_all_node_ids()
+        self.site_attrs = self._set_site_attrs()
+        self._set_propeties()
+
+    def _set_all_node_ids(self):
+        """Initialize zdd nodes using element-lattice mapping."""
+        nodes = []
+        begin = 0
+        for l, elements in enumerate(self.elements_lattice):
+            end = begin + self.n_sites[l]
+            for ele_idx in elements:
+                for site_idx in range(begin, end):
+                    nodes.append(_compose_node(site_idx, ele_idx))
+            begin = end
+        nodes = sorted(nodes)
+        return nodes
+
+    def _set_site_attrs(self):
+        """Initialize site attributes."""
+        if not self.one_of_k_rep:
+            elements_dd_exclude = self._set_excluding_elements_dd(self.elements_lattice)
+
+        site_attr = []
+        for s in range(self.n_total_sites):
+            nodes = [i for i in self.nodes if _decompose_node_to_site(i) == s]
+            ele = [_decompose_node_to_element(n) for n in nodes]
+
+            ele_dd = sorted(set(ele) - set(self.inactive_elements))
+            if not self.one_of_k_rep:
+                ele_dd = sorted(set(ele) - set(elements_dd_exclude))
+
+            site_attr.append(ZDDSite(s, ele, ele_dd))
+        return site_attr
+
+    def _set_excluding_elements_dd(self, elements_lattice):
+        """Exclude ids of unnecessary elements when one_of_k = False."""
+        elements_dd_exclude = []
+        for ele1 in self.elements_lattice:
+            common = False
+            for ele2 in self.elements_lattice:
+                if tuple(ele1) != tuple(ele2) and len(set(ele1) & set(ele2)) > 0:
+                    common = True
+                    break
+            if not common:
+                elements_dd_exclude.append(ele1[-1])
+        return elements_dd_exclude
+
+    def _set_propeties(self):
+        """Initialize active nodes, sites, and elements."""
+        self.active_nodes = sorted(
+            [_compose_node(s.site_idx, e) for s in self.site_attrs for e in s.ele_dd]
+        )
+        self.inactive_nodes = sorted(set(self.nodes) - set(self.active_nodes))
+
+        self.sites = [s.site_idx for s in self.site_attrs]
+        self.active_sites = sorted([s.site_idx for s in self.site_attrs if s.active])
+
+        self.elements = sorted(set([e for s in self.site_attrs for e in s.ele]))
+        self.active_elements = [e for s in self.site_attrs if s.active for e in s.ele]
+        self.active_elements = sorted(set(self.active_elements))
+        self.active_elements_dd = [e for s in self.site_attrs for e in s.ele_dd]
+        self.active_elements_dd = sorted(set(self.active_elements_dd))
+        return self
+
+    def print_lattice(self):
+        """Print lattice attributes."""
+        print("Sites:         ", self.sites, flush=True)
+        print("Elements:      ", self.elements, flush=True)
+        print("Nodes:         ", self.nodes, flush=True)
+        print("Lattice:", flush=True)
+        for s, attr in enumerate(self.site_attrs):
+            print("- site:        ", s, flush=True)
+            print("  elements:    ", attr.ele, flush=True)
+            print("  elements_zdd:", attr.ele_dd, flush=True)
+
+        print("Active:", flush=True)
+        print("- sites:       ", self.active_sites, flush=True)
+        print("- elements:    ", self.active_elements, flush=True)
+        print("- elements_zdd:", self.active_elements_dd, flush=True)
+        print("- nodes:       ", self.active_nodes, flush=True)
+
+        return self
 
 
 class ZddLattice:
@@ -43,107 +192,38 @@ class ZddLattice:
         """
         self._verbose = verbose
         self._n_total_sites = sum(n_sites)
-        self._elements = sorted(set([e2 for e1 in elements_lattice for e2 in e1]))
-        self._n_elements = len(self._elements)
-        if self._verbose:
-            print("Elements:", self._elements, flush=True)
 
         self._min_n_elements = min_n_elements
         self._one_of_k_rep = one_of_k_rep
-        if self._min_n_elements == 1:
-            self._one_of_k_rep = True
+        # if self._min_n_elements == 1:
+        #     self._one_of_k_rep = True
 
         if inactive_elements is None:
-            self._inactive_elements = []
-        else:
-            self._inactive_elements = inactive_elements
+            inactive_elements = []
 
-        #  initialization of self.nodes and related attributes
-        self.nodes = []
-        for l, elements in enumerate(elements_lattice):
-            begin = sum(n_sites[:l])
-            end = begin + n_sites[l]
-            for ele_idx in elements:
-                for site_idx in range(begin, end):
-                    self.nodes.append(self.compose_node(site_idx, ele_idx))
-        self.nodes = sorted(self.nodes)
-
-        self.site_attr, self.active_nodes, self.element_orbit = self.set_site_attr(
-            elements_lattice
+        self._site_set = ZDDSiteSet(
+            n_sites=n_sites,
+            elements_lattice=elements_lattice,
+            one_of_k_rep=one_of_k_rep,
+            inactive_elements=inactive_elements,
         )
+        if self._verbose:
+            self._site_set.print_lattice()
 
-        self.active_site_attr = [
-            site for site in self.site_attr if len(site.ele_dd) > 0
-        ]
-        self.inactive_nodes = sorted(set(self.nodes) - set(self.active_nodes))
+        self._n_elements = len(self._site_set.elements)
+        self._element_orbit = self._set_element_orbit()
 
-        self.sites = list(range(self.n_total_sites))
-        self.active_sites = [s.idx for s in self.site_attr if len(s.ele_dd) > 0]
-        self.active_sites = sorted(self.active_sites)
-        self.inactive_sites = sorted(set(self.sites) - set(self.active_sites))
-        self.active_elements_dd = [e for s in self.site_attr for e in s.ele_dd]
-        self.active_elements_dd = sorted(set(self.active_elements_dd))
-        self.active_elements = [
-            e for s in self.site_attr if len(s.ele_dd) > 0 for e in s.ele
-        ]
-        self.active_elements = sorted(set(self.active_elements))
-
-    #    def convert_occupation_to_element(self, occupation):
-    #
-    #        max_lattice_id = max([oc2 for oc1 in occupation for oc2 in oc1])
-    #        elements_lattice = [[] for i in range(max_lattice_id + 1)]
-    #        for e, oc1 in enumerate(occupation):
-    #            for oc2 in oc1:
-    #                elements_lattice[oc2].append(e)
-    #        elements_lattice = [sorted(e1) for e1 in elements_lattice]
-    #
-    #        return elements_lattice
-
-    def set_excluding_elements_dd(self, elements_lattice):
-
-        elements_dd_exclude = []
-        for ele1 in elements_lattice:
-            common = False
-            for ele2 in elements_lattice:
-                if tuple(ele1) != tuple(ele2) and len(set(ele1) & set(ele2)) > 0:
-                    common = True
-                    break
-            if common == False:
-                elements_dd_exclude.append(ele1[-1])
-
-        return elements_dd_exclude
-
-    def set_site_attr(self, elements_lattice=None):
-
-        if self.one_of_k_rep == False:
-            elements_dd_exclude = self.set_excluding_elements_dd(elements_lattice)
-
-        site_attr, active_nodes = [], []
+    def _set_element_orbit(self):
+        """Initialize orbits of elements."""
         uniq_ele = set()
-        for s in range(self.n_total_sites):
-            nodes = [i for i in self.nodes if self.get_site(i) == s]
-            ele = [self.get_element(n) for n in nodes]
-            if self.one_of_k_rep == False:
-                ele_dd = sorted(set(ele) - set(elements_dd_exclude))
-            else:
-                ele_dd = sorted(set(ele) - set(self.inactive_elements))
+        for site in self._site_set.site_attrs:
+            uniq_ele.add((tuple(site.ele), tuple(site.ele_dd)))
 
-            site = Site(s, ele, ele_dd)
-            site_attr.append(site)
+        element_orbit = self._find_element_orbit(uniq_ele)
+        return element_orbit
 
-            for e in ele_dd:
-                node = self.compose_node(site.idx, e)
-                active_nodes.append(node)
-
-            uniq_ele.add((tuple(ele), tuple(ele_dd)))
-            print(" site", s, ": elements =", ele, ": elements(dd) =", ele_dd)
-
-        element_orbit = self.find_element_orbit(uniq_ele)
-
-        return site_attr, sorted(active_nodes), element_orbit
-
-    def find_element_orbit(self, uniq_ele):
-
+    def _find_element_orbit(self, uniq_ele: set):
+        """Find element orbit from unique elements."""
         uniq_ele = sorted(uniq_ele)
         orbit_id = list(range(len(uniq_ele)))
         for i, j in itertools.combinations(orbit_id, 2):
@@ -165,40 +245,28 @@ class ZddLattice:
 
         return element_orbit
 
-    def compose_node(self, site_idx, element_idx):
-        return int(element_idx * 1000 + site_idx)
-
-    def decompose_node(self, node_idx):
-        site_idx = self.get_site(node_idx)
-        element_idx = self.get_element(node_idx)
-        return site_idx, element_idx
-
-    def get_element(self, node_idx):
-        element_idx = int(node_idx / 1000)
-        return element_idx
-
-    def get_elements(self, active=True, dd=True):
-        if active and dd:
-            return self.active_elements_dd
-        elif active and dd == False:
-            return self.active_elements
-        return self.elements
-
-    def get_element_orbit(self, dd=False):
-        if dd == True:
-            return self.element_orbit
-        return [ele for ele, _ in self.element_orbit]
-
-    def get_site(self, node_idx):
-        site_idx = int(node_idx % 1000)
-        return site_idx
-
-    def get_sites(self, active=True):
+    def get_sites(self, active: bool = True):
+        """Return sites."""
         if active:
-            return self.active_sites
-        return self.sites
+            return self._site_set.active_sites
+        return self._site_set.sites
 
-    def get_edge_rep(self, node_idx):
+    def get_elements(self, active: bool = True, dd: bool = True):
+        """Return elements."""
+        if active and dd:
+            return self._site_set.active_elements_dd
+        elif active and not dd:
+            return self._site_set.active_elements
+        return self._site_set.elements
+
+    def get_element_orbit(self, dd: bool = False):
+        """Return orbits of elements."""
+        if dd:
+            return self._element_orbit
+        return [ele for ele, _ in self._element_orbit]
+
+    def get_edge_rep(self, node_idx: int):
+        """Return edge representation of given node."""
         return (node_idx, node_idx)
 
     def get_nodes(
