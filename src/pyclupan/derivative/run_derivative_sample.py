4410477ccbf834357d1a4f8936c2b0a6f824f42f
Atsuto Seko <sekocha@gmail.com>
2025-01-15 18:33:58 +0900

    Implement derivative sampling.

---
+++ src/pyclupan/derivative/run_derivative_sample.py
@@ -0,0 +1,21 @@
+"""Functions for sampling derivative structures."""
+
+# import time
+# from typing import Optional
+#
+# import numpy as np
+# from pypolymlp.core.data_format import PolymlpStructure
+
+from pyclupan.derivative.derivative_utils import Derivatives
+
+
+def run_sampling_from_derivatives(
+    derivs_all: list[Derivatives],
+    verbose: bool = False,
+):
+    """Enumerate derivative structures.
+
+    Parameters
+    ----------
+    """
+    pass
