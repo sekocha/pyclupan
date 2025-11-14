2595dfce2caf566999321c7824864cdde2e54957
Atsuto Seko <sekocha@gmail.com>
2025-11-14 17:36:45 +0900

    Developing spin polynomials

--- tests/tests_features/test_spin.py
+++ tests/tests_features/test_spin.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import numpy as np
 
-from pyclupan.features.spin_polynomial import gram_schmidt
+from pyclupan.core.spin import gram_schmidt
 
 cwd = Path(__file__).parent
 
