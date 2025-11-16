bcfae4a78bab89c02d42c638e6335751d4f653f4
Atsuto Seko <sekocha@gmail.com>
2025-11-16 15:54:05 +0900

    Update tests

---
+++ tests/tests_core/test_spglib.py
@@ -0,0 +1,84 @@
+"""Tests of spglib_utils."""
+
+from pathlib import Path
+
+import numpy as np
+
+from pyclupan.core.pypolymlp_utils import Poscar, supercell
+from pyclupan.core.spglib_utils import get_permutation, get_rotations, get_symmetry
+
+cwd = Path(__file__).parent
+
+
+def test_symmetry():
+    """Test get_rotations and get_symmetry."""
+    unitcell = Poscar(str(cwd) + "/poscar-fcc").structure
+    rotations = get_rotations(unitcell)
+    rotations2, translations = get_symmetry(unitcell)
+    np.testing.assert_allclose(rotations, rotations2, atol=1e-8)
+    np.testing.assert_allclose(translations, 0.0, atol=1e-8)
+
+
+def test_permutation():
+    """Test permutation."""
+    unitcell = Poscar(str(cwd) + "/poscar-fcc").structure
+    hnf = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 4]])
+    sup = supercell(unitcell, supercell_matrix=hnf)
+    permutation = get_permutation(sup, superperiodic=False)
+    permutation_true = np.array(
+        [
+            [0, 1, 2, 3],
+            [0, 3, 2, 1],
+            [0, 3, 2, 1],
+            [0, 1, 2, 3],
+            [0, 3, 2, 1],
+            [0, 1, 2, 3],
+            [0, 1, 2, 3],
+            [0, 3, 2, 1],
+            [0, 3, 2, 1],
+            [0, 1, 2, 3],
+            [0, 1, 2, 3],
+            [0, 3, 2, 1],
+            [3, 0, 1, 2],
+            [3, 2, 1, 0],
+            [3, 2, 1, 0],
+            [3, 0, 1, 2],
+            [3, 2, 1, 0],
+            [3, 0, 1, 2],
+            [3, 0, 1, 2],
+            [3, 2, 1, 0],
+            [3, 2, 1, 0],
+            [3, 0, 1, 2],
+            [3, 0, 1, 2],
+            [3, 2, 1, 0],
+            [2, 3, 0, 1],
+            [2, 1, 0, 3],
+            [2, 1, 0, 3],
+            [2, 3, 0, 1],
+            [2, 1, 0, 3],
+            [2, 3, 0, 1],
+            [2, 3, 0, 1],
+            [2, 1, 0, 3],
+            [2, 1, 0, 3],
+            [2, 3, 0, 1],
+            [2, 3, 0, 1],
+            [2, 1, 0, 3],
+            [1, 2, 3, 0],
+            [1, 0, 3, 2],
+            [1, 0, 3, 2],
+            [1, 2, 3, 0],
+            [1, 0, 3, 2],
+            [1, 2, 3, 0],
+            [1, 2, 3, 0],
+            [1, 0, 3, 2],
+            [1, 0, 3, 2],
+            [1, 2, 3, 0],
+            [1, 2, 3, 0],
+            [1, 0, 3, 2],
+        ]
+    )
+    np.testing.assert_equal(permutation, permutation_true)
+
+    _, perm_lt = get_permutation(sup, superperiodic=True, hnf=hnf)
+    perm_lt_true = np.array([[0, 1, 2, 3], [3, 0, 1, 2], [2, 3, 0, 1], [1, 2, 3, 0]])
+    np.testing.assert_equal(perm_lt, perm_lt_true)
