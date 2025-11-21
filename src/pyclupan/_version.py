3b7211d67758a11b057be9373caa20c6c9eb9720
Atsuto Seko <sekocha@gmail.com>
2025-11-21 14:35:14 +0900

    Implement composition

--- src/pyclupan/_version.py
+++ src/pyclupan/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.0"
-__version_tuple__ = version_tuple = (0, 1, 0)
+__version__ = version = "0.1.0.post1"
+__version_tuple__ = version_tuple = (0, 1, 0, "post1")
 
-__commit_id__ = commit_id = "g3bfaa7615"
+__commit_id__ = commit_id = "g8beb3cc71"
---
+++ src/pyclupan/core/composition.py
@@ -0,0 +1,93 @@
+"""Class for defining compositions."""
+
+import numpy as np
+
+
+class Composition:
+    """Class for defining compositions."""
+
+    def __init__(self, chemical_comps_end_members: np.ndarray):
+        """Init method.
+
+        Parameters
+        ----------
+        chemical_comps_end_members: Chemical compositions for end members.
+            shape=(n_end_members, n_type),
+            Each row corresponds to number of atoms for each end member.
+            For example, if SnO and SnO2 are endmembers that are used
+            to define the composition, this array should be given as
+            chemical_comps_end_members = [[1, 1], [1, 2]].
+        """
+        self._composition_axis = np.array(chemical_comps_end_members).T
+        self._composition_axis_inv = np.linalg.pinv(self._composition_axis)
+        self._proj = self._composition_axis @ self._composition_axis_inv
+
+        self._energies_end = None
+
+    def get_composition(self, n_atoms: np.ndarray):
+        """Return composition.
+
+        Parameter
+        ---------
+        n_atoms: Number of atoms in structure, shape = (n_type).
+        """
+        if not np.allclose(self._proj @ n_atoms, n_atoms):
+            raise RuntimeError("The composition not in the given compositional space.")
+
+        partition = self._composition_axis_inv @ n_atoms
+        comp = partition / np.sum(partition)
+        return comp, partition
+
+    def get_compositions(self, n_atoms_array: np.ndarray):
+        """Return compositions.
+
+        Parameter
+        ---------
+        n_atoms_array: Numbers of atoms in structures.
+                       shape = (n_str, n_type)
+        """
+        n_atoms = np.array(n_atoms_array).T
+        if not np.allclose(self._proj @ n_atoms, n_atoms):
+            raise RuntimeError("The composition not in the given compositional space.")
+
+        partition = self._composition_axis_inv @ n_atoms
+        partition = partition.T
+        comp = partition / np.sum(partition, axis=1)[:, None]
+        return comp, partition
+
+    def compute_formation_energy(self, energy: float, n_atoms: np.ndarray):
+        """Compute formation energy.
+
+        Energy per n_atoms must be given.
+        """
+        _, partition = self.get_composition(n_atoms)
+        e_end = self._energies_end @ partition
+        return (energy - e_end) / np.sum(partition)
+
+    def compute_formation_energies(
+        self,
+        energies: np.ndarray,
+        n_atoms_array: np.ndarray,
+    ):
+        """Compute formation energy.
+
+        Energy per n_atoms must be given.
+        """
+        _, partition = self.get_compositions(n_atoms_array)
+        e_end = self._energies_end @ partition.T
+        return (energies - e_end) / np.sum(partition, axis=1)
+
+    @property
+    def energies_end_members(self):
+        """Return energies of end members."""
+        return self._energies_end
+
+    @energies_end_members.setter
+    def energies_end_members(self, e: np.ndarray):
+        """Setter of energies of end members.
+
+        Energy per chemical composition must be given.
+        """
+        if self._composition_axis.shape[1] != len(e):
+            raise RuntimeError("Size of Energies for end members inconsitent.")
+        self._energies_end = e
