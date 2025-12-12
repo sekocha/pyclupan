13d1a64c4e906a139ef92c1c37f75e77d89f1587
Atsuto Seko <sekocha@gmail.com>
2025-12-12 16:56:50 +0900

    Implement workflow for uploading pypi

--- src/pyclupan/_version.py
+++ src/pyclupan/_version.py
@@ -27,7 +27,5 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.1.post8"
-__version_tuple__ = version_tuple = (0, 1, 1, "post8")
-
-__commit_id__ = commit_id = "gd25a8dda7"
+__version__ = version = "0.1.2"
+__version_tuple__ = version_tuple = (0, 1, 2)
