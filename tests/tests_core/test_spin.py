4035f6460dc43ee9e2c547b281fcbe1236fa1e71
Atsuto Seko <sekocha@gmail.com>
2025-12-17 16:43:35 +0900

    Update tests

--- tests/tests_core/test_spin.py
+++ tests/tests_core/test_spin.py
@@ -4,9 +4,31 @@ from pathlib import Path
 
 import numpy as np
 
-from pyclupan.core.spin import eval_cluster_functions
+from pyclupan.core.spin import eval_cluster_functions, gram_schmidt
 
 cwd = Path(__file__).parent
+
+
+def test_gram_schmidt():
+    """Test gram_schmidt."""
+    spins = [1, 0, -1]
+    cons = gram_schmidt(spins)
+    mat = np.array([np.polyval(cons.T, s) for s in spins])
+    metric = mat.T @ mat
+    np.testing.assert_allclose(metric[0][1], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[0][2], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[1][2], 0.0, atol=1e-8)
+
+    spins = [2, 1, 0, -1]
+    cons = gram_schmidt(spins)
+    mat = np.array([np.polyval(cons.T, s) for s in spins])
+    metric = mat.T @ mat
+    np.testing.assert_allclose(metric[0][1], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[0][2], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[0][3], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[1][2], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[1][3], 0.0, atol=1e-8)
+    np.testing.assert_allclose(metric[2][3], 0.0, atol=1e-8)
 
 
 def test_eval_cluster_functions():
