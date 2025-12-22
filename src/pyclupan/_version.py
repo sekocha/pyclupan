f7d1b5dfab25f2e11ba25245f03c089c68749992
Atsuto Seko <sekocha@gmail.com>
2025-12-22 12:43:48 +0900

    Update pyproject

--- src/pyclupan/_version.py
+++ src/pyclupan/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.3.post2"
-__version_tuple__ = version_tuple = (0, 1, 3, "post2")
+__version__ = version = "0.1.3.post31"
+__version_tuple__ = version_tuple = (0, 1, 2, "post31")
 
-__commit_id__ = commit_id = "ge1bb11ea9"
+__commit_id__ = commit_id = "gc8d176914"
