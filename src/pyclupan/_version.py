40427b2d36c9fe94d69c1ebd8970b35b3cef4f7d
Atsuto Seko <sekocha@gmail.com>
2026-09-21 18:17:08 +0900

    Implement structures with displacements

--- src/pyclupan/_version.py
+++ src/pyclupan/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.2.1"
-__version_tuple__ = version_tuple = (0, 2, 1)
+__version__ = version = "0.2.2"
+__version_tuple__ = version_tuple = (0, 2, 2)
 
 __commit_id__ = commit_id = "g4dbd09e34"
